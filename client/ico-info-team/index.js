590249e9995814bd666674653ad0241ba942e6ce
Serghey Soloviov <thatmean@gmail.com>
2018-01-15 18:48:32 +0200

    team fix

--- client/ico-info-team/index.js
+++ client/ico-info-team/index.js
@@ -3,11 +3,9 @@ new Vue({
   template:
   `
   <div class="">
-    <br>
     <div class="owl-carousel">
       <div>
-        <div class="team">
-          <img src="https://s3.amazonaws.com/icofaces/13bcabb8b9eb954b4ce7a92de6c7ee9638004d30.jpg">
+        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/13bcabb8b9eb954b4ce7a92de6c7ee9638004d30.jpg')">
           <div class="team_info">
             <h5>CEO</h5>
             <h3>Sasha Ivanov</h3>
@@ -18,8 +16,7 @@ new Vue({
         </div>
       </div>
       <div>
-        <div class="team">
-          <img src="https://s3.amazonaws.com/icofaces/03d471300effbea2f9e681eadc804cdb3b564134.jpg">
+        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/03d471300effbea2f9e681eadc804cdb3b564134.jpg')">
           <div class="team_info">
             <h5>CEO</h5>
             <h3>Vitalik Buterin</h3>
@@ -30,8 +27,7 @@ new Vue({
         </div>
       </div>
       <div>
-        <div class="team">
-          <img src="https://s3.amazonaws.com/icofaces/9ee1b5be9a66f6713dbc84192328a95d3a16336e.jpg">
+        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/9ee1b5be9a66f6713dbc84192328a95d3a16336e.jpg')">
           <div class="team_info">
             <h5>CEO</h5>
             <h3>Patrick Dai</h3>
@@ -73,21 +69,7 @@ new Vue({
       center: true,
       items:2,
       loop:true,
-      margin:10,
-      responsive:{
-          320:{
-              items:1
-          },
-          640:{
-              items:2
-          },
-          720:{
-              items:3
-          },
-          1200:{
-              items:4
-          }
-      }
+      margin:6
     });
   },
   methods: {
