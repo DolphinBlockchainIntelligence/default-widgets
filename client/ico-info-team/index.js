2276d25b13ad812a251c13568f67767c552e92dd
Eugen Soloviov <suenot@gmail.com>
2018-01-15 20:48:00 +0300

    boilerplate for lite widgets

--- client/ico-info-team/index.js
+++ client/ico-info-team/index.js
@@ -2,48 +2,46 @@ new Vue({
   el: '#vue-container',
   template:
   `
-  <div class="">
-    <div class="owl-carousel">
-      <div>
-        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/13bcabb8b9eb954b4ce7a92de6c7ee9638004d30.jpg')">
-          <div class="team_info">
-            <h5>CEO</h5>
-            <h3>Sasha Ivanov</h3>
-            <a href="#"><i class="flaticon-social-media-2"></i></a>
-            <a href="#"><i class="flaticon-social-media-1"></i></a>
-            <a href="#"><i class="flaticon-social-media"></i></a>
-          </div>
+  <div class="owl-carousel">
+    <div>
+      <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/13bcabb8b9eb954b4ce7a92de6c7ee9638004d30.jpg')">
+        <div class="team_info">
+          <h5>CEO</h5>
+          <h3>Sasha Ivanov</h3>
+          <a href="#"><i class="flaticon-social-media-2"></i></a>
+          <a href="#"><i class="flaticon-social-media-1"></i></a>
+          <a href="#"><i class="flaticon-social-media"></i></a>
         </div>
       </div>
-      <div>
-        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/03d471300effbea2f9e681eadc804cdb3b564134.jpg')">
-          <div class="team_info">
-            <h5>CEO</h5>
-            <h3>Vitalik Buterin</h3>
-            <a href="#"><i class="flaticon-social-media-2"></i></a>
-            <a href="#"><i class="flaticon-social-media-1"></i></a>
-            <a href="#"><i class="flaticon-social-media"></i></a>
-          </div>
+    </div>
+    <div>
+      <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/03d471300effbea2f9e681eadc804cdb3b564134.jpg')">
+        <div class="team_info">
+          <h5>CEO</h5>
+          <h3>Vitalik Buterin</h3>
+          <a href="#"><i class="flaticon-social-media-2"></i></a>
+          <a href="#"><i class="flaticon-social-media-1"></i></a>
+          <a href="#"><i class="flaticon-social-media"></i></a>
         </div>
       </div>
-      <div>
-        <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/9ee1b5be9a66f6713dbc84192328a95d3a16336e.jpg')">
-          <div class="team_info">
-            <h5>CEO</h5>
-            <h3>Patrick Dai</h3>
-            <a href="#"><i class="flaticon-social-media-2"></i></a>
-            <a href="#"><i class="flaticon-social-media-1"></i></a>
-            <a href="#"><i class="flaticon-social-media"></i></a>
-          </div>
+    </div>
+    <div>
+      <div class="team" style="background-image: url('https://s3.amazonaws.com/icofaces/9ee1b5be9a66f6713dbc84192328a95d3a16336e.jpg')">
+        <div class="team_info">
+          <h5>CEO</h5>
+          <h3>Patrick Dai</h3>
+          <a href="#"><i class="flaticon-social-media-2"></i></a>
+          <a href="#"><i class="flaticon-social-media-1"></i></a>
+          <a href="#"><i class="flaticon-social-media"></i></a>
         </div>
       </div>
-
     </div>
   </div>
   `,
   data: function data() {
     return {
-      project: {}
+      project: {},
+      items: 2
     }
   },
   mounted () {
@@ -57,6 +55,7 @@ new Vue({
       return decodeURIComponent(results[2].replace(/\+/g, ' '))
     }
     id = getParameterByName('id', window.location.href)
+    this.items = getParameterByName('items', window.location.href)
     axios.get('/base/' + id, {
     }).then((response) => {
       console.log(response.data)
@@ -67,9 +66,9 @@ new Vue({
     })
     $(".owl-carousel").owlCarousel({
       center: true,
-      items:2,
-      loop:true,
-      margin:6
+      items: this.items,
+      loop: true,
+      margin: 0
     });
   },
   methods: {
