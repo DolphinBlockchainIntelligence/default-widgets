44d5fa091b6c4edd41b95d64bb89543ff02fc044
Serghey Soloviov <thatmean@gmail.com>
2018-01-21 18:05:47 +0200

    ico-info partial data

--- client/ico-info/index.js
+++ client/ico-info/index.js
@@ -25,14 +25,16 @@ new Vue({
               </p>
               <h6>The formal business entity</h6>
               <p>{{project.formal | ifEmpty}}</p>
-            </div>
-            <div class="project_info_descr">
               <h6>Project homeland</h6>
               <p>{{project.homeland | ifEmpty}}</p>
+            </div>
+            <div class="project_info_descr">
               <h6>Project platform</h6>
-              <p>{{project.coins | ifEmpty}}</p>
+              <p>{{project.platform | ifEmpty}}</p>
+              <h6>Project status</h6>
+              <p>{{project.status | ifEmpty}}</p>
               <h6>Description</h6>
-              <p>{{project.tokenDescription | ifEmpty}}</p>
+              <p>{{project.description | ifEmpty}}</p>
             </div>
           </div>
           <div class="project_actions">
@@ -47,13 +49,16 @@ new Vue({
       </div>
       <div class="tabs_content_wrap" id="2">
         <h6>Name of the token</h6>
-        <p>{{project.symbol | ifEmpty}}</p>
+        <p>{{project.tokenName | ifEmpty}}</p>
 
         <h6>Token's ticker code</h6>
         <p>{{project.symbol | ifEmpty}}</p>
 
         <h6>Project platform</h6>
-        <p>{{project.coins | ifEmpty}}</p>
+        <p>{{project.tokenPlatform | ifEmpty}}</p>
+
+        <h6>Your token gives the right</h6>
+        <p>{{project.tokenDescription | ifEmpty}}</p>
 
         <h6>Total emission</h6>
         <p>{{project.emission | ifEmpty}}</p>
@@ -61,18 +66,21 @@ new Vue({
         <h6>Distribution of tokens</h6>
         <p>{{project.distribution | ifEmpty}}</p>
 
-        <h6>Your token gives the right</h6>
-        <p>{{project.tokenDescription | ifEmpty}}</p>
+        <h6>Extra emission</h6>
+        <p>{{project.extraemission | ifEmpty}}</p>
       </div>
       <div class="tabs_content_wrap" id="3">
-      <h6>Legal Entity Name</h6>
-      <p>-</p>
-      <h6>Legal Entity Location</h6>
-      <p>-</p>
-      <h6>Legal Entity ID</h6>
-      <p>-</p>
-      <h6>Date of registration</h6>
-      <p>-</p>
+        <h6>Legal Entity Name</h6>
+        <p>{{project.legalname | ifEmpty}}</p>
+
+        <h6>Legal Entity Location</h6>
+        <p>{{project.legallocation | ifEmpty}}</p>
+
+        <h6>Legal Entity ID</h6>
+        <p>{{project.legalid | ifEmpty}}</p>
+
+        <h6>Date of registration</h6>
+        <p>-</p>
       </div>
       <div class="tabs_content_wrap" id="4">
         <div class="team">
@@ -109,47 +117,12 @@ new Vue({
         </div>
       </div>
       <div class="tabs_content_wrap" id="5">
-        <div class="events">
-          <div class="event open">
-            <div class="event_head">
-              <span class="event_type">Sale</span>
-              <span class="event_start">1 Jul 2017</span>
-              <span class="event_end">15 Jul 2017</span>
-              <div class="event_arrow">
-                <span><i class="icon-down-open"></i></span>
-              </div>
-            </div>
-            <div class="event_body">
-              <div class="event_info">
-                <h6>Softcap</h6>
-                <p>{{project.category}}</p>
-                <h6>Token to sell</h6>
-                <p>500000</p>
-                <h6>Hardcap</h6>
-                <p>{{project.category}}</p>
-                <h6>Soft/hardcap coin</h6>
-                <p>{{project.category}}</p>
-                <h6>Acceptable coins</h6>
-                <p>ETH</p>
-                <h6>Actually raised</h6>
-                <p>-</p>
-                <h6>Actual finish date</h6>
-                <p>01.10.2017</p>
-              </div>
-              <div class="event_descr">
-                <h6>special requirements</h6>
-                <p>Our token sale fits KYC/AML procedures.</p>
-                <p>Investors from list of country are not permitted to participate in the token sale Token sale participants will be required to register with an email No special requirements or restrictions. The sale is open to everyone. Unsure or undecided</p>
-                <p>Other:</p>
-              </div>
-            </div>
-            
-          </div>
+        <div class="events" v-for="sale in project.sales">
           <div class="event">
             <div class="event_head">
-              <span class="event_type">Presale</span>
-              <span class="event_start">1 Jul 2017</span>
-              <span class="event_end">15 Jul 2017</span>
+              <span class="event_type">{{sale.type}}</span>
+              <span class="event_start">{{sale.start}}</span>
+              <span class="event_end">{{sale.end}}</span>
               <div class="event_arrow">
                 <span><i class="icon-down-open"></i></span>
               </div>
@@ -157,25 +130,30 @@ new Vue({
             <div class="event_body">
               <div class="event_info">
                 <h6>Softcap</h6>
-                <p>{{project.category | ifEmpty}}</p>
+                <p>{{sale.softCap}}</p>
                 <h6>Token to sell</h6>
-                <p>500000</p>
+                <p>{{sale.tosell}}</p>
                 <h6>Hardcap</h6>
-                <p>{{project.category | ifEmpty}}</p>
-                <h6>Soft/hardcap coin</h6>
-                <p>{{project.category | ifEmpty}}</p>
+                <p>{{sale.hardCap}}</p>
+                <!-- <h6>Soft/hardcap coin</h6>
+                <p>{{project.category}}</p> -->
                 <h6>Acceptable coins</h6>
-                <p>ETH</p>
-                <td>Actually raised</td>
-                <td>-</td>
-                <td>Actual finish date</td>
-                <td>01.10.2017</td>
+                <p>
+                  <span class="badge badge-pill badge-info" v-for="coin in sale.acceptable">{{coin}}</span>
+                </p>
+                <h6>Actually raised</h6>
+                <p>{{sale.raised}}</p>
+                <h6>Actual finish date</h6>
+                <p>{{sale.real}}</p>
+
+                <h6>ERC20 token address</h6>
+                <p>{{sale.erc20addr}}</p>
+                <h6>Average price</h6>
+                <p>{{sale.course}} BTC</p>
               </div>
               <div class="event_descr">
                 <h6>special requirements</h6>
-                <p>Our token sale fits KYC/AML procedures.</p>
-                <p>Investors from list of country are not permitted to participate in the token sale Token sale participants will be required to register with an email No special requirements or restrictions. The sale is open to everyone. Unsure or undecided</p>
-                <p>Other:</p>
+                <p>{{sale.requirements}}</p>
               </div>
             </div>
           </div>
@@ -218,9 +196,9 @@ new Vue({
       $(target).addClass('active')
     })
 
-    $('.event_head').on('click', function(){
-      $(this).parent().toggleClass('open')
-    })
+    $(document).on('click', '.event_head', function(){
+     $(this).parent().toggleClass('open')
+    });
   },
   computed: {
     projectComputed: function() {
