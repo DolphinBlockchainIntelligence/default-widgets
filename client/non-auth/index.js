89ea4ac7606898a73570b7f7731a9c3d0e828eb9
Ksiusha <ksiusha5@gmail.com>
2018-01-24 10:18:20 +0200

    non-auth

---
+++ client/non-auth/index.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#vue-container',
+  template:
+  `
+    <table class="table">
+      <tr>
+        <td>
+          Please <a href="/#/auth" target="_top">log in</a> or <a href="/#/auth">sign up</a><br>
+          to add widgets you want
+        </td>
+      </tr>
+    </table>
+  `
+})
