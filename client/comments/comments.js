e0e672e1e4886749595e69ab9adac1d008c4fd5e
Eugen Soloviov <suenot@gmail.com>
2018-01-25 21:44:39 +0300

    +sentiments widgets

---
+++ client/comments/comments.js
@@ -0,0 +1,79 @@
+var colors = ['#f98a83', '#989898', '#85f77e']
+new Vue({
+  el: '#comments',
+  template: 
+  `
+  <div id="sentiments-comments" class="sentiments-comments">
+    <h3 v-if="heading">Comments:</h3>
+    <ul class="comments">
+        <li v-for="(comment, key) in comments" :class="'comment sentiment'+comment.Sentiment">
+        <div class="heading">
+          <div class="author">{{ comment.user }}:</div>
+          <div class="date">{{ comment.date }}:</div>
+        </div>
+        <a :href="'https://bitcointalk.org/index.php?topic='+comment.topicId+'.msg'+key+'#msg'+key" target="_blank" class="text">{{ comment.text }}:</a>
+      </li>
+    </ul>
+    <p align="center">
+      <button class="large button hide-for-small-only" @click.prevent="toShowAll($event)">Show all</button>
+    </p>
+  </div>
+  `,
+  data: function data() {
+    return {
+      commentsSource: [],
+      showNumber: 30,
+      showAll: false,
+      heading: ''
+    }
+  },
+  mounted () {
+    this.loadComments()
+  },
+  computed: {
+    comments () {
+      var comments = this.commentsSource
+      comments = _.orderBy(comments, ['date'], ['desc'])
+      comments = _.forEach(comments, function(item, i) {
+        item.date = moment(item.date).calendar()
+      })
+      if (!this.showAll) {
+        const pick = (obj, keys) =>
+          Object.keys(obj)
+            .slice(0, keys)
+            .reduce((acc, key) => {
+              acc[key] = obj[key];
+              return acc;
+            }, {})
+        comments = pick(comments, this.showNumber)
+      }
+      return comments
+    }
+  },
+  methods: {
+    loadComments: function () {
+      var getParameterByName = function(name, url) {
+        if (!url) url = window.location.href
+        name = name.replace(/[\[\]]/g, "\\$&")
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url)
+        if (!results) return null
+        if (!results[2]) return ''
+        return decodeURIComponent(results[2].replace(/\+/g, " "))
+      }
+      var postId = getParameterByName('id', window.location.href)
+      this.heading = getParameterByName('heading', window.location.href)
+      axios.get('/data/btt-sentiments/D'+ postId +'.json')
+      .then(response => {
+        this.commentsSource = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    },
+    toShowAll: function (event) {
+      this.showAll = true
+      event.currentTarget.classList.add('hide')
+    }
+  }
+})
---
+++ client/linechart/linechart.js
@@ -0,0 +1,94 @@
+var colors = ['#f98a83', '#989898', '#85f77e']
+new Vue({
+  el: '#linechart',
+  template: 
+  `
+  <div>
+    <h3 v-if="heading">{{heading}}</h3>
+    <div id="sentimentsLineChart" style="min-width: 100%; width: 100%; height: 100vh; min-height: 100vh; margin: 0 auto"></div>
+  </div>
+  `,
+  data: function data() {
+    return {
+      heading: '',
+    }
+  },
+  mounted () {
+    this.sentimentsLineChart()
+  },
+  methods: {
+    sentimentsLineChart: function () {
+      var highchartContainer = document.getElementById('sentimentsLineChart')
+      var seriesOptions = []
+      var seriesCounter = 0
+      var createChart = function () {
+        Highcharts.stockChart(highchartContainer, {
+          chart: {
+            spacingBottom: 30
+          },
+          legend: {
+            enabled: true,
+            floating: true,
+            verticalAlign: 'bottom',
+            align:'center',
+            y: 30
+          },
+          rangeSelector: {
+            selected: 4
+          },
+          plotOptions: {
+            series: {
+              showInNavigator: true
+            },
+          },
+          series: seriesOptions
+        })
+      }
+      var getParameterByName = function(name, url) {
+        if (!url) url = window.location.href
+        name = name.replace(/[\[\]]/g, '\\$&')
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url)
+        if (!results) return null
+        if (!results[2]) return ''
+        return decodeURIComponent(results[2].replace(/\+/g, ' '))
+      }
+      var postId = getParameterByName('id', window.location.href)
+      this.heading = getParameterByName('heading', window.location.href)
+      var names = ['positive', 'neutral', 'negative']
+      var component = this
+      axios.get('/data/btt-sentiments/S'+ postId +'.json')
+      .then(response => {
+        var data = response.data
+        var chartData = {
+          negative: [],
+          neutral: [],
+          positive: [],
+          pointStart: ''
+        }
+        var dates = []
+        var names = ['negative', 'neutral', 'positive']
+        chartData.negative = data.chart.negative
+        chartData.neutral = data.chart.neutral
+        chartData.positive = data.chart.positive
+        chartData.pointStart = data.pointStart
+        names.forEach(function (name, i) {
+          seriesOptions[i] = {
+            name: name,
+            data: chartData[name],
+            color: colors[i],
+            pointStart: chartData.pointStart * 10,
+            pointInterval: 3600 * 1000 * 24
+          }
+          seriesCounter += 1
+          if (seriesCounter === names.length) {
+            createChart()
+          }
+        })
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+  }
+})
