dca15907c4827f760845a512238a56aaf59267fb
Eugen Soloviov <suenot@gmail.com>
2018-02-12 14:02:57 +0300

    itrend

--- client/itrend/index.js
+++ client/itrend/index.js
@@ -4,52 +4,46 @@ new Vue({
   `
   <div class="itrend-widgets">
 
-
+    // peaks
     <template v-if="id == '5a78a6d15b82e50001b5ae12'">
       <div class="itrend-widget">
         <h3>Data</h3>
-        <p>Social mentions: <strong>55</strong></p>
-        <p>People: <strong>37</strong></p>
-        <p>Links shared: <strong>21</strong></p>
-        <p>Images shared: <strong>3</strong></p>
+        <p>Social mentions: <strong>1</strong></p>
+        <p>People: <strong>1</strong></p>
+        <p>Links shared: <strong>0</strong></p>
+        <p>Images shared: <strong>1</strong></p>
         <p>News found: <strong>0</strong></p>
         <p>Terms tracked: <strong>1</strong></p>
         <div class="itrend-widget-ico" style="background-image: url('img/data.svg');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Timeline</h3>
-        <p>Today: <strong>0</strong> mentions</p>
-        <p>This week: <strong>11</strong> mentions <strong class="negative">-75%</strong></p>
-        <p>This month: <strong>55</strong> mentions</p>
+        <p>Today: <strong>1</strong> mentions <strong class="negative">-50%</strong></p>
+        <p>This week: <strong>6</strong> mentions <strong class="negative">-57%</strong></p>
+        <p>This month: <strong>21</strong> mentions</p>
         <div class="itrend-widget-ico" style="background-image: url('img/timeline.svg');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Largest audiences</h3>
-        <p>Rabobank: <strong>149,379</strong></p>
-        <p>Dr.Shivarajkumar FC: <strong>11,084</strong></p>
-        <p>Business Insider NL: <strong>8,036</strong></p>
-        <p>Herwin Thole: <strong>2,954</strong></p>
-        <p>Baaz Magazine: <strong>1,995</strong></p>
+        <p>Pawan Naidu: <strong>7</strong></p>
         <div class="itrend-widget-ico" style="background-image: url('img/people.svg');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Most engaging authors</h3>
-        <p>Peaks: <strong>50</strong> replies</p>
-        <p>KKDSFC: <strong>13</strong> replies</p>
-        <p>Rabobank: <strong>3</strong> replies</p>
-        <p>The Podium: <strong>3</strong> replies</p>
-        <p>Stella Braber: <strong>3</strong> replies</p>
+        <p>Ram Ayyapp: <strong>1</strong> replies</p>
+        <p>Varun Tej: <strong>1</strong> replies</p>
+        <p>Peaks: <strong>1</strong> replies</p>
         <div class="itrend-widget-ico" style="background-image: url('img/chat.svg');"></div>
       </div>
     </template>
 
-
+    // tellow
     <template v-if="id == '5a74890501bf07000150410c'">
       <div class="itrend-widget">
         <h3>Data</h3>
-        <p>Social mentions: <strong>6</strong></p>
-        <p>People: <strong>5</strong></p>
-        <p>Links shared: <strong>4</strong></p>
+        <p>Social mentions: <strong>0</strong></p>
+        <p>People: <strong>0</strong></p>
+        <p>Links shared: <strong>0</strong></p>
         <p>Images shared: <strong>0</strong></p>
         <p>News found: <strong>0</strong></p>
         <p>Terms tracked: <strong>1</strong></p>
@@ -58,35 +52,35 @@ new Vue({
       <div class="itrend-widget">
         <h3>Timeline</h3>
         <p>Today: <strong>0</strong> mentions</p>
-        <p>This week: <strong>4</strong> mentions <strong class="positive">+100%</strong></p>
-        <p>This month: <strong>6</strong> mentions</p>
+        <p>This week: <strong>1</strong> mentions <strong class="negative">-75%</strong></p>
+        <p>This month: <strong>5</strong> mentions</p>
         <div class="itrend-widget-ico" style="background-image: url('img/timeline-128.gif');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Largest audiences</h3>
-        <p>Martijn Pennekamp: <strong>8,391</strong></p>
-        <p>Starten.nl: <strong>390</strong></p>
-        <p>Sijbrand Tieleman: <strong>379</strong></p>
-        <p>Stella Braber: <strong>214</strong></p>
-        <p>Peaks: <strong>191</strong></p>
-        <div class="itrend-widget-ico" style="background-image: url('img/audience-128.gif');"></div>
+        // <p>Martijn Pennekamp: <strong>8,391</strong></p>
+        // <p>Starten.nl: <strong>390</strong></p>
+        // <p>Sijbrand Tieleman: <strong>379</strong></p>
+        // <p>Stella Braber: <strong>214</strong></p>
+        // <p>Peaks: <strong>191</strong></p>
+        // <div class="itrend-widget-ico" style="background-image: url('img/audience-128.gif');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Most engaging authors</h3>
-        <p>Rabobank: <strong>3</strong> replies</p>
-        <p>Peaks: <strong>3</strong> replies</p>
-        <p>Stella Braber: <strong>2</strong> replies</p>
-        <div class="itrend-widget-ico" style="background-image: url('img/engage-128.gif');"></div>
+        // <p>Rabobank: <strong>3</strong> replies</p>
+        // <p>Peaks: <strong>3</strong> replies</p>
+        // <p>Stella Braber: <strong>2</strong> replies</p>
+        // <div class="itrend-widget-ico" style="background-image: url('img/engage-128.gif');"></div>
       </div>
     </template>
 
-
+    // headstart
     <template v-if="id == '5a73628f8f45f500017ebb7b'">
       <div class="itrend-widget">
         <h3>Data</h3>
-        <p>Social mentions: <strong>7</strong></p>
-        <p>People: <strong>7</strong></p>
-        <p>Links shared: <strong>12</strong></p>
+        <p>Social mentions: <strong>3</strong></p>
+        <p>People: <strong>3</strong></p>
+        <p>Links shared: <strong>1</strong></p>
         <p>Images shared: <strong>0</strong></p>
         <p>News found: <strong>0</strong></p>
         <p>Terms tracked: <strong>1</strong></p>
@@ -94,24 +88,24 @@ new Vue({
       </div>
       <div class="itrend-widget">
         <h3>Timeline</h3>
-        <p>Today: <strong>7</strong> mentions <strong class="positive">+600%</strong></p></p>
-        <p>This week: <strong>12</strong> mentions <strong class="positive">+9%</strong></p>
-        <p>This month: <strong>24</strong> mentions</p>
+        <p>Today: <strong>3</strong> mentions <strong class="negative">-84%</strong></p></p>
+        <p>This week: <strong>61</strong> mentions <strong class="positive">+1,120%</strong></p>
+        <p>This month: <strong>66</strong> mentions</p>
         <div class="itrend-widget-ico" style="background-image: url('img/timeline-128.gif');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Largest audiences</h3>
-        <p>mocho17: <strong>90,898</strong></p>
-        <p>G. Amsterdam: <strong>2,179</strong></p>
-        <p>leogarzon: <strong>418</strong></p>
-        <p>tim moorehead: <strong>389</strong></p>
-        <p>Liz: <strong>120</strong></p>
+        <p>G. Amsterdam: <strong>2,181</strong></p>
+        <p>CryptoHolland: <strong>272</strong></p>
+        <p>f0nky: <strong>9</strong></p>
         <div class="itrend-widget-ico" style="background-image: url('img/audience-128.gif');"></div>
       </div>
       <div class="itrend-widget">
         <h3>Most engaging authors</h3>
-        <p>mocho17: <strong>5</strong> replies</p>
-        <p>CryptOrangutang: <strong>1</strong> replies</p>
+        <p>ICO HeadStart: <strong>2</strong> replies</p>
+        <p>MrMoorfield: <strong>1</strong> replies</p>
+        <p>tehMoonwalkeR: <strong>1</strong> replies</p>
+        <p>Liz: <strong>1</strong> replies</p>
         <div class="itrend-widget-ico" style="background-image: url('img/engage-128.gif');"></div>
       </div>
     </template>
